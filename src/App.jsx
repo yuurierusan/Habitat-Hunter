a09e1d1c09ec8659a2012a75e803fc24c652d7fe
Jonshea Nutson <yuurieru@icloud.com>
2023-03-28 10:54:12 -0400

    tailwind dependency ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
 
-function App() {
-    return <div className='App'></div>
+export default function App() {
+    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
 }
-
-export default App
