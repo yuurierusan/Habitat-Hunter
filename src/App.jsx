d68e73127089884487140c0b95f325427851ee30
Jonshea Nutson <yuurieru@icloud.com>
2023-03-28 12:26:51 -0400

    mkdir components and touch navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import './App.css'
-
 export default function App() {
-    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    return (
+        <div className='App'>
+            {/* <Navbar /> */}
+            {/* <Filters/> */}
+            {/* <Listings/> */}
+            {/* <Footer/> */}
+        </div>
+    )
 }
