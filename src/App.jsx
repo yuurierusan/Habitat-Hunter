f28e17872e83e2ee0b5d3e85abe38ba826eb3360
Jonshea Nutson <yuurieru@icloud.com>
2023-03-29 16:45:20 -0400

    added socials to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <div></div>
+            <Footer />
         </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import { BsTwitter, BsInstagram, BsFacebook } from 'react-icons/bs'
+
+const Footer = () => {
+    const icons = [<BsTwitter />, <BsInstagram />, <BsFacebook />]
+    return (
+        <div className='bg-white border-t-2 shadow-md  shadow-gray-300 sticky bottom-0 h-20 w-full flex items-center justify-center gap-6'>
+            {icons.map((icon) => (
+                <div className='text-[30px] text-gray-600 hover:text-black duration-100 ease-out '>
+                    {icon}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Footer
