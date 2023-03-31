1d23327d3d13e8088ea7180dcd1fefa1a99be338
Jonshea Nutson <yuurieru@icloud.com>
2023-03-31 18:16:08 -0400

    working on making calls to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
-import { CheckSession } from './services/Auth'
+// import { CheckSession } from './services/Auth'
 import { useState, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Listings from './components/Listings'
-import FourOFour from './pages/404'
+// import FourOFour from './pages/404'
 
 const App = () => {
     const [user, setUser] = useState(null)
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     const handleLogOut = () => {
         setUser(null)
@@ -26,23 +26,23 @@ const App = () => {
         setUser(user)
     }
 
-    userEffect(() => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            checkToken()
-        }
-    }, [])
+    // userEffect(() => {
+    //     const token = localStorage.getItem('token')
+    //     if (token) {
+    //         checkToken()
+    //     }
+    // }, [])
 
     return (
         <div>
-            <Routes>
-                <Navbar />
-                <div>
-                    <Listings />
-                </div>
-                <Footer />
-                <Route path='*' element={<FourOFour />} />
-            </Routes>
+            {/* <Routes> */}
+            <Navbar />
+            <div>
+                <Listings />
+            </div>
+            <Footer />
+            {/* <Route path='*' element={<FourOFour />} /> */}
+            {/* </Routes> */}
         </div>
     )
 }
